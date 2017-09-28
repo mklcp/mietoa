433da77301a00777d43db4401b4c870ae46184a4
melopee <melopee@users.noreply.github.com>
2017-09-28 15:09:11 -0500

    new command: glosser; new field in the dict: g_en; new search option in the webint': gloss;; correction of some bugs

--- bot.js
+++ bot.js
@@ -6,14 +6,7 @@ const camxes_preproc = require('./camxes_preproc.js')
 const camxes_postproc = require('./camxes_postproc.js')
 
 const authorized_channels = ["general","chiejo","off-topic"] //personnal info
-client.login('your-discord-token'); //personnal info
-
-const eskptel = "[*A-Za-z0-9\\(\\)]*:? ?"
-const receskptel = new RegExp(eskptel)
-const coha_cmd = "^" + eskptel
-const cmd = ["\\.m","\\.s","\\.p","\\.help","\\.channels","\\.g"]
-const recmd = [new RegExp(coha_cmd+cmd[0]+" "),new RegExp(coha_cmd+cmd[1]+" "),new RegExp(coha_cmd+cmd[2]+" "),new RegExp(coha_cmd+cmd[3]+"$"),new RegExp(coha_cmd+cmd[4]+"$"),new RegExp(coha_cmd+cmd[5]+" ")]
-const gochiaitao = {"shea":"_fỏa ji/ shẻa tỏatōa. go kuì bû dải tủa jí_","sia":"_tỉduāshaō go tu dó_","buo":"go bủo ka"}
+client.login('discord-token-of-the-bot'); //personnal info
 
 client.on('ready', () => {
   console.log(gochiaitao.buo);
@@ -33,7 +26,7 @@ client.on('message', msg => {
         if (!untonecz) { toa = undiak(toa); }
         for (i in data) {
           if ((toaqcz && ((!untonecz?data[i].toaq_undia:data[i].toaq).indexOf(toa)+1)) || (encz && (data[i].d_en.indexOf(toa)+1)) || (typecz && (data[i].t_en.indexOf(toa)+1))) {
-            sendtoaq(msg,i); found_match++;
+            sendtoaq(msg,i); found_match++; break;
           }
         }
         if (!found_match) { msg.channel.send(gochiaitao.sia) }
@@ -56,7 +49,7 @@ client.on('message', msg => {
         if (!untonecz) { toa = undiak(toa); }
         for (i in data) {
           if ((!untonecz?data[i].toaq_undia:data[i].toaq)==toa) {
-            sendtoaq(msg,i); found_match++;
+            sendtoaq(msg,i); found_match++; break;
           }
         }
         if (!found_match) { msg.channel.send(gochiaitao.sia) }
@@ -81,7 +74,7 @@ client.on('message', msg => {
       ,deskx(cmd[2],"return a prettified parse of a toaq text.")
       ,deskx(cmd[3],"return the description of the bot " + client.user.username + " and its options.")
       ,deskx(cmd[4],"return the channels where " + client.user.username + " is active.")
-      ,deskx(cmd[5],"return the gloss of the text. Like `.p` and unlike `.s` and `.m`, it supports both the so-called \"tone marks\" and diacritics.")
+      ,deskx(cmd[5],"return the gloss of the text. Like `.p` and unlike `.s` and `.m`, it supports both the so-called \"tone marks\" and diacritics. Possible options: `m` aligns each word of the gloss and each word of the input in a monospace font.")
       ,"This bot also works in Private Messages."
       ].map(function(x){msg.channel.send(x);});
       return;
@@ -93,14 +86,21 @@ client.on('message', msg => {
     }
 
     if (recmd[5].test(toatoa)) {
-      var toa = toatoa.replace(receskptel,"").replace(/\s+/," ").slice(cmd[5].length)
+      var tao = toatoa.replace(receskptel,"").replace(/\s+/," ").slice(cmd[5].length); var opt = tao.substring(0,tao.indexOf(" "));
+      if (opt.indexOf("+")+1) {
+        var monocz = opt.indexOf("m")+1;
+        var toa = tao.slice(tao.indexOf(" ")+1);
+      } else {
+        var toa = tao;
+      }
       if (toa) {
-        res = [];
-        taotao = camxes_preproc.preprocessing(toa).split(' ').filter(function(x){return !(x==='')})
-        for (t in taotao) {
-          res.push(glosser(taotao[t]))
+        toa=camxes_preproc.preprocessing(toa).split(' ').filter(function(x){return !(x==='')}); gw=glosser(toa);
+        if (monocz) {
+          d=normaspa(toa,gw.map(spofuroda))
+          msg.channel.send("```"+d[0].join(" ")+"\n"+d[1].join(" ")+"```");
+        } else {
+          msg.channel.send(gw.join(" "));
         }
-        msg.channel.send(res.join(" "));
       } else {
         msg.channel.send(gochiaitao.shea)
       }
@@ -108,8 +108,38 @@ client.on('message', msg => {
     }
 }});
 
+function normaspa(a,b) {
+  var res = [[],[]];
+  var lm = (a.length>b.length?b:a)
+  for (i in lm) {
+    d=a[i].length-b[i].length
+    res[0].push(d>0?a[i]:a[i]+" ".repeat(Math.abs(d)));
+    res[1].push(d>0?b[i]+" ".repeat(d):b[i]);
+  }
+  return res
+}
+
+const eskptel = "[*A-Za-z0-9\\(\\)]*:? ?"
+const receskptel = new RegExp(eskptel)
+const coha_cmd = "^" + eskptel
+const cmd = ["\\.m","\\.s","\\.p","\\.help","\\.channels","\\.g"]
+const recmd = [new RegExp(coha_cmd+cmd[0]+" "),new RegExp(coha_cmd+cmd[1]+" "),new RegExp(coha_cmd+cmd[2]+" "),new RegExp(coha_cmd+cmd[3]+"$"),new RegExp(coha_cmd+cmd[4]+"$"),new RegExp(coha_cmd+cmd[5]+" ")]
+const gochiaitao = {"shea":"⟦_fỏa ji/ shẻa tỏatōa. go kuì bû dải tủa jí_⟧","sia":"⟦_tỉduāshaō go tu dó_⟧","buo":"go bủo ka","bu":"⟦_bủdūa_⟧"}
+
+ttf = {"?":function(d){return "do/is."+d}
+      ,"\/":function(d){return "that.which."+d}
+      ,"~":function(d){return d+"-ly"}
+      ,"\^":function(d){return "which."+d}
+      ,"V":function(d){return "that."+d}
+      ,"\\":function(d){return "on."+d}
+      ,"-":function(d){return d+"-esque"}
+      ,"∅":function(d){return "\\*"+d+"\\*"}}
+
+tone_marks = Object.keys(ttf)
+
 function sendtoaq(msg,i) {msg.channel.send(data[i].toaq + ", _" + data[i].t_en + "_: " + rivbiroda(data[i].d_en));}
 function rivbiroda(str) { return str.split('').map(function(x) { if (["_","*","\\"].includes(x)) {return "\\"+x } else {return x}}).join('') } // escape markdown tokens, since toaq's dict' use some of them
+function spofuroda(str) { a=(typeof str === 'string'); if(a) {str=str.split('')} b=str.filter(function(x) { return !((["_","\\"]).includes(x)) }); if (a) {b=b.join('')}; return b }
 function deskx(it,as) {return "`"+it+"`"+": "+as}
 function undiak(toa) { return toa.replace(/[áàâāãǎả]/g,"a").replace(/[éèêēẽěẻ]/g,"e").replace(/[íìîīĩǐỉ]/g,"i").replace(/[óòôōõǒỏ]/g,"o").replace(/[úùûūũǔủ]/g,"u");}
 function camxit(str) {
@@ -117,35 +147,24 @@ function camxit(str) {
 	return camxes_postproc.postprocessing(JSON.stringify(res, undefined, 2), 1)
 }
 
-ttf = {"?":function(d){return "do."+d}
-      ,"-":function(d){return "do."+d}
-      ,"\/":function(d){return "that.which."+d}
-      ,"~":function(d){return d+"-ly"}
-      ,"\^":function(d){return "which."+d}
-      ,"V":function(d){return "that."+d}
-      ,"\\":function(d){return "on."+d}}
-tone_marks = Object.keys(ttf)
-
-function glosser(w) {
-  var dahitonw = w[w.length-1]; var notow = camxes_preproc.toaq_tone_marks_to_diacritics(w.replace(new RegExp("[/\\\\~^?V]","g"),"").toLowerCase()); var found_match=0; var ceres;
-  for (i in data) {
-    if (data[i].toaq==notow) {
-      ceres=i; found_match++;
-    }
+function glosser(taotao) {
+  res = [];
+  for (t in taotao) {
+    t=taotao[t]; var dahitonw = "∅"; for (i in t) { if (tone_marks.includes(t[i])) {dahitonw=t[i]; break;} }
+    var notow = t.replace(new RegExp("[-/\\\\~^?V]","g"),"").toLowerCase();
+    var found_match=0; var ceres;
+    for (i in data) { if (data[i].toaq_undia==notow) { ceres=i; found_match++; break; } }
+    var dc=data[ceres]; if (!found_match) { res.push(gochiaitao.bu) } else { res.push((ttf[dahitonw])((dc.g_en?dc.g_en:"⟦?⟧"))) }
   }
-  if (!found_match) { return "_bủdūa_" }
-  if (!tone_marks.includes(dahitonw)) {
-    return "\\*\\*λ"+data[ceres].toaq+"λ\\*\\*"
-  } else {
-    return (ttf[dahitonw])("λ"+data[ceres].toaq+"λ")
-  }
+  return res
 }
+// input: ".g shảo shaỏ shao? sháo shaó shao/ shào shaò shao\ shâo shaô shao^ shǎo shaǒ shaoV shão shaõ shao~ shao shāo shaō shao- shao?hui- shảohūi shao?hūi shảohui- shāohui?"
+// expected output: "do.λshaoλ do.λshaoλ do.λshaoλ that.which.λshaoλ that.which.λshaoλ that.which.λshaoλ on.λshaoλ on.λshaoλ on.λshaoλ which.λshaoλ which.λshaoλ which.λshaoλ that.λshaoλ that.λshaoλ that.λshaoλ λshaoλ-ly λshaoλ-ly λshaoλ-ly **λshaoλ** λshaoλ-esque λshaoλ-esque λshaoλ-esque do.λshaohuiλ do.λshaohuiλ do.λshaohuiλ do.λshaohuiλ λshaohuiλ-esque
 
 function pretty_parse(str) {
   return camxes_postproc.prettify(str
     .replace(/\[([A-Za-z0-9_\(\)'])+ /g, "[")
     .replace(/\[ +/g, "[").replace(/ +\]/g, "]")
-    .replace(/([A-Z] )\[/g, "$1[")
-    )
+    .replace(/([A-Z] )\[/g, "$1["))
     .replace(/ +/gm, " ");
 }
--- readme.md
+++ readme.md
@@ -12,10 +12,19 @@ Some utilities for the [toaq](http://toaq.org) language, featuring:
   + launch with:
     + `node bot.js`, in a terminal with nodejs. Just fill up `bot.js` with the bot's parameters ⦅names of the authorized channels; and the bot's discord token⦆. Before any launchs, install the dependencies ⦅currently, only `discord.js`⦆ by hand, or with `npm install`
 
-### generate the dictionary in a json format
+##### generate the dictionary in a json format
+###### [toaq.org/dictionary](toaq.org/dictionary)
 ```bash
 wget toaq.org/dictionary/; perl -i -p -e 's/\<\!DOCTYPE(.*)\n//g;' -e 's?<tr><td>(.*)</td><td><em>(.*)</td></tr><tr><td>(.*)</td></tr>?\{toaq:$1,t_en:$2,d_en:$3?g;' -e 's/\"/\\"/g;' -e 's/\{toaq:(.*),t_en:(.*),d_en:(.*)/\{"toaq":"$1","t_en":"$2","d_en":"$3"/g;' index.html;
 cat index.html | grep '{"toaq"' > undex.html; cat index.html | grep -oP '{"toaq":.*,"t_en"' | sed -E 's/[áàâāãǎả]/a/g;s/[éèêēẽěẻ]/e/g;s/[íìîīĩǐỉ]/i/g;s/[óòôōõǒỏ]/o/g;s/[úùûūũǔủ]/u/g;s/\{\"toaq\"\:/\"toaq_undia\":/g;s/",".*/"\},/g;' > undia;
 paste -d ',' undex.html undia > toaq_dict.js; rm undia; rm undex.html; rm index.html; perl -i -p -e 'print "data = [\n" if $. == 1' toaq_dict.js; echo "]" >> toaq_dict.js; perl -i -p -e 's/\r\",\"/\",\"/g;' toaq_dict.js
 ```
+
+###### [toaq.org/dict.html](toaq.org/dict.html)
+```
+wget http://toaq.org/dict.html; perl -i -p -e 's?</style>?</style>\n?g;s?<tr?\n<tr?g;s/<meta.*>\n//g;s/<style.*<\/style>\n//g;s/.*<thead>.*\n//;s?.*<\/thead>.*\n??g;s?.*freezebar\-cell.*\n??g;s/<tr.+?>/<tr>/g;s/<td.+?>/<td>/g;s/<th.+?>/<th>/g;s/<div.+?>/<div>/g;;s/<th>.*<\/th>//g;s/<div>(.*)<\/div>/$1/g;s/<\/?div>//g;s?<tr><td>(.*)</td><td>(.*)</td><td>(.*)</td><td>(.*)</td></tr>?{toaq:$1,t_en:$2,g_en:$3,d_en:$4?g;s/\"/\\"/g;s?</tbody>??g;s?</table>??g;s/<a.*<\/a>//g;s?{toaq:(.*),t_en:(.*),g_en:(.*),d_en:(.*)?{"toaq":"$1","t_en":"$2","g_en":"$3","d_en":"$4"?g;' dict.html
+cat dict.html | grep '{"toaq"' > undex.html; cat undex.html | grep -oP '{"toaq":.*,"t_en"' | sed -E 's/[áàâāãǎả]/a/g;s/[éèêēẽěẻ]/e/g;s/[íìîīĩǐỉ]/i/g;s/[óòôōõǒỏ]/o/g;s/[úùûūũǔủ]/u/g;s/\{\"toaq\"\:/\"toaq_undia\":/g;s/",".*/"\},/g;' > undia;
+paste -d ',' undex.html undia > toaq_dict.js; rm undia; rm undex.html; rm dict.html; perl -i -p -e 'print "data = [\n" if $. == 1' toaq_dict.js; echo "]" >> toaq_dict.js; perl -i -p -e 's/\r\",\"/\",\"/g;' toaq_dict.js
+```
+
 ⦅Not necessary to use either the dict' or the bot, but useful.⦆
